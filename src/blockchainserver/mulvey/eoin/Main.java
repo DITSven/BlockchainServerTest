a60a70031800555269053d46a3d66a983bd2d9c4
DITSven <c14752305@mydit.ie>
2017-11-23 03:29:21 +0000

    Working for testing purposes.

--- src/blockchainserver/mulvey/eoin/Main.java
+++ src/blockchainserver/mulvey/eoin/Main.java
@@ -4,7 +4,7 @@ public class Main {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		new ChainServer("D:\\College\\Project\\javawork\\BlockchainPrototype\\prototypeChain.chain");
+		new ChainServer("D:\\College\\Project\\javawork\\BlockchainPrototype\\prototypeChain.chain");//to be changed to relative location
 		
 	}
 
